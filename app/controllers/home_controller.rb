39979786d553e58df7c219b771372b1a10bb193b
Daniel Oliveira Filho <daniel@matrix.local>
2015-11-07 15:13:12 -0200

    Leads

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,33 @@
 class HomeController < ApplicationController
   def index
 
-    @leads = []
+  #  @leads = []
 
-    if current_user
+  #  if current_user
 
-        client = Restforce.new :api_version => "32.0", :oauth_token => current_user.oauth_token,
-          :refresh_token => current_user.refresh_token,
-          :instance_url  => current_user.instance_url,
-          :client_id     => '3MVG9KI2HHAq33RxE3uJ6fN7r8Ni2mAGzpVhlQeKePV7GxYdNGe65yXkxVk',
-          :client_secret => '915198885628210263'
+  #      client = Restforce.new :api_version => "32.0", :oauth_token => current_user.oauth_token,
+  #        :refresh_token => current_user.refresh_token,
+  #        :instance_url  => current_user.instance_url,
+  #        :client_id     => '3MVG9KI2HHAq33RxE3uJ6fN7r8Ni2mAGzpVhlQeKePV7GxYdNGe65yXkxVk',
+  #        :client_secret => '915198885628210263'
 
          #@newLead = client.create('Lead', FirstName: 'Daniel', MiddleName: 'Oliveira', LastName: 'Filho', Status: 'New', Company: 'xdd');
 
-         @leads = client.query("select id, name, status, company from Lead");
+  #       @leads = client.query("select id, name, status, company from Lead");
 
-    end
+    #end
 
   end
 
   def about
   end
 
-  private
+  def leads
+    @leads = "Lista de Leads"
 
+  end
+
+  private
   def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/about' => 'home#about'
-
+  get '/leads' => 'home#leads'
+  
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
