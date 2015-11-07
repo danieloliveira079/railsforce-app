2cf8a4bc2674acd450e7314df0ba0449433a1797
Daniel Oliveira Filho <daniel@matrix.local>
2015-11-07 13:47:30 -0200

    Updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,21 @@
 class HomeController < ApplicationController
   def index
-    client = Restforce.new :api_version => "32.0", :oauth_token => current_user.oauth_token,
-      :refresh_token => current_user.refresh_token,
-      :instance_url  => current_user.instance_url,
-      :client_id     => '3MVG9KI2HHAq33RxE3uJ6fN7r8Ni2mAGzpVhlQeKePV7GxYdNGe65yXkxVk',
-      :client_secret => '915198885628210263'
 
-    @newLead = client.create('Lead', FirstName: 'Daniel', MiddleName: 'Oliveira', LastName: 'Filho', Status: 'New', Company: 'xdd');
+    @leads = []
 
-    @leads = client.query("select id, name, status, company from Lead");
+    if current_user
+
+        client = Restforce.new :api_version => "32.0", :oauth_token => current_user.oauth_token,
+          :refresh_token => current_user.refresh_token,
+          :instance_url  => current_user.instance_url,
+          :client_id     => '3MVG9KI2HHAq33RxE3uJ6fN7r8Ni2mAGzpVhlQeKePV7GxYdNGe65yXkxVk',
+          :client_secret => '915198885628210263'
+
+         #@newLead = client.create('Lead', FirstName: 'Daniel', MiddleName: 'Oliveira', LastName: 'Filho', Status: 'New', Company: 'xdd');
+
+         @leads = client.query("select id, name, status, company from Lead");
+
+    end
 
   end
 
@@ -19,7 +26,7 @@ class HomeController < ApplicationController
 
   def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+  end
     helper_method :current_user
 
 end
