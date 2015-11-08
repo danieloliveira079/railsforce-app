4bdf3edfbe050fc172827590eed49aafbd7178ed
Daniel Oliveira Filho <daniel@matrix.local>
2015-11-08 18:09:51 -0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Lead.create([{name: 'John', last_name: 'Hills', email: 'john.hills@gmail.com', company: 'Green Field', job_title: 'CEO', phone: '+44 87677788', website: 'http://www.greenfield.com', status: 'New' },
+             {name: 'Mary', last_name: 'Jane', email: 'mary.jane@gmail.com', company: 'New Marketing', job_title: 'Sales', phone: '+33 564464664', website: 'http://www.newmarketing.com', status: 'New' },
+             {name: 'Paul', last_name: 'Adams', email: 'paul.adams@hotmail.com', company: 'Orange Juice', job_title: 'Project Manager', phone: '+44 423223223', website: 'http://www.orangejuice.com', status: 'New' },
+             {name: 'Ozzy', last_name: 'Osbourne', email: 'ozzy@mail666.com', company: 'Ozzy Company', job_title: 'Director', phone: '+66 6666666', website: 'http://www.ozzyosbourne.com', status: 'New' },
+             {name: 'Jimmy', last_name: 'Page', email: 'jimi.page@ledzeppelin.com', company: 'Led Zeppelin', job_title: 'Lead Guitar', phone: '+33 564464664', website: 'http://www.ledzeppelin.com', status: 'New' },
+             {name: 'Jim', last_name: 'Morrison', email: 'jimi.page@ledzeppelin.com', company: 'The Doors', job_title: 'Singer', phone: '+55 78737236', website: 'http://www.thedoors.com', status: 'New' } ])
