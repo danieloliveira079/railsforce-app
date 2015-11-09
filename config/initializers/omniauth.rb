99bf297dd73a0f1ee142bce15a44c4631cf21935
Daniel Oliveira Filho <daniel@matrix.local>
2015-11-08 22:52:32 -0200

    config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,15 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
-  appSettings = AppSetting.where(active:true).take
-
-  if appSettings
-
-    Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :salesforce, appSettings.client_id, appSettings.client_secret
-    end
-
-  else
     Rails.application.config.middleware.use OmniAuth::Builder do
       provider :salesforce, '', ''
     end
-  end
